410eb01d7646f08b4c8dede4afd52d824a3838a5
Vitalina Shapovalova <vladdislavovna@gmail.com>
2024-03-22 09:56:21 +0200

    Merge f_2. Add features.

--- Groups/Implementions/SubjectEntity.cs
+++ Groups/Implementions/SubjectEntity.cs
@@ -2,5 +2,12 @@ namespace Groups.Implementions;
 
 public class SubjectEntity
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
 
+    public SubjectEntity(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
 }
