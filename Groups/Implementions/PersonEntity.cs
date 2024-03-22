f05bfb60016165a449b8ee7cfbe87294d5108b64
Vitalina Shapovalova <vladdislavovna@gmail.com>
2024-03-22 09:53:06 +0200

    Merge f_1. Add features.

--- Groups/Implementions/PersonEntity.cs
+++ Groups/Implementions/PersonEntity.cs
@@ -2,5 +2,14 @@ namespace Groups.Implementions;
 
 public class PersonEntity
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
 
+    public PersonEntity(int id, string name, string email)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+    }
 }
